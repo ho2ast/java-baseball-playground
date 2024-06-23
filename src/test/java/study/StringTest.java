300e2ae34edd753bd0c8cb0c197fb1e732b0893c
ho2ast <lucid7@hotmail.co.kr>
2024-06-23 15:13:38 +0900

    feat: charAt() 테스트
    String charAt()메서드 테스트, assertThatThrownBy을 통해 예외 발생 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -30,6 +32,22 @@ public class StringTest {
         String actual = "(1,2)";
         String substring = actual.substring(1, 4);
         assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String actual = "abc";
+        char charAtFirst = actual.charAt(0);
+        assertThat(charAtFirst).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt() 메서드 사용시 유효하지 않은 인덱스를 사용할 때 예외 발생")
+    void outBoundExceptionWhenUsingCharAt() {
+        String actual = "abc";
+
+        assertThatThrownBy(() -> actual.charAt(3))
+            .isInstanceOf(IndexOutOfBoundsException.class);
     }
 
 }
